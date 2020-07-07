7c7524c710e613c65476c8dbf74ebe2ab4db5edf
Devifish <devifish@outlook.com>
2020-07-07 16:19:54 +0800

    add: 统一ResponseBody数据返回格式

--- fish-common/fish-common-core/src/main/java/cn/devifish/cloud/common/core/config/WebMvcConfiguration.java
+++ fish-common/fish-common-core/src/main/java/cn/devifish/cloud/common/core/config/WebMvcConfiguration.java
@@ -2,16 +2,25 @@ package cn.devifish.cloud.common.core.config;
 
 import cn.devifish.cloud.common.core.constant.DateTimeConstant;
 import cn.devifish.cloud.common.core.convert.ConverterEnumFactory;
-import cn.devifish.cloud.common.core.handler.RestfulResponseMethodProcessorAspect;
+import cn.devifish.cloud.common.core.handler.RestfulResponseMethodProcessor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * WebMvcConfiguration
@@ -20,7 +29,6 @@ import java.time.format.DateTimeFormatter;
  * @author Devifish
  * @date 2020/7/2 15:20
  */
-@Import({RestfulResponseMethodProcessorAspect.class})
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnWebApplication(type = Type.SERVLET)
 public class WebMvcConfiguration implements WebMvcConfigurer {
@@ -38,5 +46,24 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         //枚举转换格式
         ConverterEnumFactory converterEnumFactory = new ConverterEnumFactory();
         registry.addConverterFactory(converterEnumFactory);
+    }
+
+    @Bean
+    @ConditionalOnBean(RequestMappingHandlerAdapter.class)
+    public InitializingBean restfulResponseMethodProcessorBean(RequestMappingHandlerAdapter adapter) {
+        return () -> {
+            List<HandlerMethodReturnValueHandler> returnValueHandlers = adapter.getReturnValueHandlers();
+            returnValueHandlers = new ArrayList<>(Objects.requireNonNull(returnValueHandlers));
+            returnValueHandlers.replaceAll(handlerMethodReturnValueHandler -> {
+                //替换Spring MVC ResponseBody默认处理方式
+                if (handlerMethodReturnValueHandler instanceof RequestResponseBodyMethodProcessor) {
+                    List<HttpMessageConverter<?>> messageConverters = adapter.getMessageConverters();
+                    return new RestfulResponseMethodProcessor(messageConverters);
+                }
+                return handlerMethodReturnValueHandler;
+            });
+
+            adapter.setReturnValueHandlers(returnValueHandlers);
+        };
     }
 }
---
+++ fish-common/fish-common-core/src/main/java/cn/devifish/cloud/common/core/handler/RestfulResponseMethodProcessor.java
@@ -0,0 +1,70 @@
+package cn.devifish.cloud.common.core.handler;
+
+import cn.devifish.cloud.common.core.RestfulEntity;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractMessageConverterMethodProcessor;
+
+import java.util.List;
+
+/**
+ * RestfulResponseMethodProcessor
+ * Restful Response转换处理器
+ * 重写RequestResponseBodyMethodProcessor
+ * 自动将Method返回值转换为 Restful API风格
+ *
+ * @author Devifish
+ * @date 2020/7/6 18:27
+ * @see org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor
+ * @see org.springframework.web.servlet.mvc.method.annotation.AbstractMessageConverterMethodProcessor
+ */
+@SuppressWarnings("NullableProblems")
+public class RestfulResponseMethodProcessor extends AbstractMessageConverterMethodProcessor {
+
+    public RestfulResponseMethodProcessor(List<HttpMessageConverter<?>> converters) {
+        super(converters);
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return false;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean supportsReturnType(MethodParameter returnType) {
+        return (AnnotatedElementUtils.hasAnnotation(returnType.getContainingClass(), ResponseBody.class) ||
+                returnType.hasMethodAnnotation(ResponseBody.class));
+    }
+
+    /**
+     * 将Spring MVC {@link ResponseBody}方法的返回数据
+     * 使用 {@link RestfulEntity} 进行包装
+     *
+     * @param returnValue Object
+     * @param returnType MethodParameter
+     * @param mavContainer ModelAndViewContainer
+     * @param webRequest NativeWebRequest
+     * @throws Exception Exception
+     */
+    @Override
+    public void handleReturnValue(Object returnValue, MethodParameter returnType, ModelAndViewContainer mavContainer, NativeWebRequest webRequest) throws Exception {
+        mavContainer.setRequestHandled(true);
+        ServletServerHttpRequest inputMessage = createInputMessage(webRequest);
+        ServletServerHttpResponse outputMessage = createOutputMessage(webRequest);
+
+        // Try even with null return value. ResponseBodyAdvice could get involved.
+        writeWithMessageConverters(RestfulEntity.ok(returnValue), returnType, inputMessage, outputMessage);
+    }
+}
