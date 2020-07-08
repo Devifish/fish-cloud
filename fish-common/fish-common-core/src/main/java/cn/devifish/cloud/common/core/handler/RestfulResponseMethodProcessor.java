fbde0b53b0c63c445de4b6488f3ee3fcb02dc9d2
Devifish <devifish@outlook.com>
2020-07-08 10:01:48 +0800

    change: 使用重写RequestResponseBodyMethodProcessor方式实现

--- fish-common/fish-common-core/src/main/java/cn/devifish/cloud/common/core/handler/RestfulResponseMethodProcessor.java
+++ fish-common/fish-common-core/src/main/java/cn/devifish/cloud/common/core/handler/RestfulResponseMethodProcessor.java
@@ -2,16 +2,15 @@ package cn.devifish.cloud.common.core.handler;
 
 import cn.devifish.cloud.common.core.RestfulEntity;
 import org.springframework.core.MethodParameter;
-import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.ModelAndViewContainer;
-import org.springframework.web.servlet.mvc.method.annotation.AbstractMessageConverterMethodProcessor;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -25,27 +24,15 @@ import java.util.List;
  * @see org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor
  * @see org.springframework.web.servlet.mvc.method.annotation.AbstractMessageConverterMethodProcessor
  */
-@SuppressWarnings("NullableProblems")
-public class RestfulResponseMethodProcessor extends AbstractMessageConverterMethodProcessor {
+public class RestfulResponseMethodProcessor extends RequestResponseBodyMethodProcessor {
 
+    /**
+     * 仅使用handleReturnValue无需使用其他参数
+     *
+     * @param converters 消息转换器
+     */
     public RestfulResponseMethodProcessor(List<HttpMessageConverter<?>> converters) {
         super(converters);
-    }
-
-    @Override
-    public boolean supportsParameter(MethodParameter parameter) {
-        return false;
-    }
-
-    @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        return null;
-    }
-
-    @Override
-    public boolean supportsReturnType(MethodParameter returnType) {
-        return (AnnotatedElementUtils.hasAnnotation(returnType.getContainingClass(), ResponseBody.class) ||
-                returnType.hasMethodAnnotation(ResponseBody.class));
     }
 
     /**
@@ -56,20 +43,21 @@ public class RestfulResponseMethodProcessor extends AbstractMessageConverterMeth
      * @param returnType   MethodParameter
      * @param mavContainer ModelAndViewContainer
      * @param webRequest   NativeWebRequest
-     * @throws Exception Exception
+     * @throws IOException                         IOException
+     * @throws HttpMediaTypeNotAcceptableException HttpMediaTypeNotAcceptableException
+     * @throws HttpMessageNotWritableException     HttpMessageNotWritableException
      */
     @Override
-    public void handleReturnValue(Object returnValue, MethodParameter returnType, ModelAndViewContainer mavContainer, NativeWebRequest webRequest) throws Exception {
-        mavContainer.setRequestHandled(true);
-        ServletServerHttpRequest inputMessage = createInputMessage(webRequest);
-        ServletServerHttpResponse outputMessage = createOutputMessage(webRequest);
+    @SuppressWarnings("NullableProblems")
+    public void handleReturnValue(
+            Object returnValue, MethodParameter returnType,
+            ModelAndViewContainer mavContainer, NativeWebRequest webRequest)
+            throws IOException, HttpMediaTypeNotAcceptableException, HttpMessageNotWritableException {
 
         // 如果返回参数为 RestfulEntity 则不再包装
         if (!(returnValue instanceof RestfulEntity)) {
             returnValue = RestfulEntity.ok(returnValue);
         }
-
-        // Try even with null return value. ResponseBodyAdvice could get involved.
-        writeWithMessageConverters(returnValue, returnType, inputMessage, outputMessage);
+        super.handleReturnValue(returnValue, returnType, mavContainer, webRequest);
     }
 }
