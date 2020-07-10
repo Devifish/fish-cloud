64b6f36ddd6a2293b78aeb1fb17ba0cf3b555719
Devifish <devifish@outlook.com>
2020-07-10 17:27:37 +0800

    change: 防止缺失UserDetailsService无法启动

--- fish-common/fish-common-security/src/main/java/cn/devifish/cloud/common/security/config/WebSecurityConfiguration.java
+++ fish-common/fish-common-security/src/main/java/cn/devifish/cloud/common/security/config/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package cn.devifish.cloud.common.security.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,10 +8,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * WebSecurityConfiguration
@@ -24,21 +27,41 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/favor.ico");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/actuator/**", "/error").permitAll()
                 .anyRequest().authenticated();
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/favor.ico");
-    }
-
+    /**
+     * 注册 AuthenticationManager
+     * OAuth2 GrantType为密码方式时依赖
+     *
+     * @return AuthenticationManager
+     * @throws Exception 异常
+     */
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    /**
+     * 默认UserDetailsService实现
+     * 防止为 NULL 时 Spring Security随机生成用户数据
+     * 已存在 UserDetailsService 实现时不加载
+     *
+     * @return UserDetailsService
+     */
+    @Bean
+    @ConditionalOnMissingBean(UserDetailsService.class)
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
     }
 
     /**
