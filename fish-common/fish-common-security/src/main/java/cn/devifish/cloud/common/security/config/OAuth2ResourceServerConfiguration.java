ba06c38116db2be68f5b6e1001bf9291ba34d670
Devifish <devifish@outlook.com>
2020-07-16 16:24:41 +0800

    change: 迁移OpenApi实现到Properties

--- fish-common/fish-common-security/src/main/java/cn/devifish/cloud/common/security/config/OAuth2ResourceServerConfiguration.java
+++ fish-common/fish-common-security/src/main/java/cn/devifish/cloud/common/security/config/OAuth2ResourceServerConfiguration.java
@@ -1,8 +1,7 @@
 package cn.devifish.cloud.common.security.config;
 
-import cn.devifish.cloud.common.security.annotation.OpenApi;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.RegExUtils;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
@@ -10,12 +9,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-import java.util.Collection;
-import java.util.regex.Pattern;
-
-import static org.springframework.core.annotation.AnnotationUtils.findAnnotation;
+import java.util.List;
 
 /**
  * OAuth2ResourceServerConfiguration
@@ -28,13 +23,12 @@ import static org.springframework.core.annotation.AnnotationUtils.findAnnotation
  */
 @EnableResourceServer
 @RequiredArgsConstructor
+@EnableConfigurationProperties(OpenApiUrlProperties.class)
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
-    private static final Pattern PATH_VARIABLE_PATTERN = Pattern.compile("\\{(.*?)}");
-
     private final TokenStore tokenStore;
-    private final RequestMappingHandlerMapping handlerMapping;
+    private final OpenApiUrlProperties openApiUrlProperties;
 
     /**
      * 加载放行路径
@@ -45,13 +39,12 @@ public class OAuth2ResourceServerConfiguration extends ResourceServerConfigurerA
      */
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        String[] ignore = {};
-        String[] openApiUrlPatterns = scannerOpenApiRequestMappingUrlPatterns();
+        List<String> openApiUrls = openApiUrlProperties.getUrls();
+        String[] ignore = openApiUrls.toArray(new String[0]);
 
         //白名单路径放行, 其他需要鉴权
         http.authorizeRequests()
                 .antMatchers(ignore).permitAll()
-                .antMatchers(openApiUrlPatterns).permitAll()
                 .anyRequest().authenticated();
     }
 
@@ -64,33 +57,6 @@ public class OAuth2ResourceServerConfiguration extends ResourceServerConfigurerA
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.tokenStore(tokenStore);
-    }
-
-    /**
-     * 扫描添加 {@link OpenApi}注解 RequestMapping接口
-     * 获取接口URL加入请求白名单
-     */
-    public String[] scannerOpenApiRequestMappingUrlPatterns() {
-        var handlerMethods = handlerMapping.getHandlerMethods();
-        var handlerMethodEntry = handlerMethods.entrySet();
-
-        //扫描Spring MVC HandlerMapping添加OpenApi注解接口并转换成UrlPattern
-        return handlerMethodEntry.stream()
-                .filter(entry -> {
-                    var handlerMethod = entry.getValue();
-                    return findAnnotation(handlerMethod.getMethod(), OpenApi.class) != null
-                            || findAnnotation(handlerMethod.getBeanType(), OpenApi.class) != null;
-                })
-                .map(entry -> {
-                    var requestMappingInfo = entry.getKey();
-                    return requestMappingInfo
-                            .getPatternsCondition()
-                            .getPatterns();
-                })
-                .flatMap(Collection::stream)
-                .map(pattern -> RegExUtils.replaceAll(pattern, PATH_VARIABLE_PATTERN, "*"))
-                .distinct()
-                .toArray(String[]::new);
     }
 
 }
---
+++ fish-common/fish-common-security/src/main/java/cn/devifish/cloud/common/security/config/OpenApiUrlProperties.java
@@ -0,0 +1,72 @@
+package cn.devifish.cloud.common.security.config;
+
+import cn.devifish.cloud.common.security.annotation.OpenApi;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.RegExUtils;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * OpenApiUrlProperties
+ *
+ * @author Devifish
+ * @date 2020/7/16 16:07
+ */
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "security.oauth2.open-api")
+public class OpenApiUrlProperties implements InitializingBean {
+
+    private static final Pattern PATH_VARIABLE_PATTERN = Pattern.compile("\\{(.*?)}");
+
+    private final RequestMappingHandlerMapping handlerMapping;
+
+    @Getter
+    @Setter
+    private List<String> urls = new ArrayList<>();
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        var openApiUrlPatterns = scannerOpenApiRequestMappingUrlPatterns();
+
+        //添加注解获取到的URL
+        urls.addAll(openApiUrlPatterns);
+    }
+
+    /**
+     * 扫描添加 {@link OpenApi}注解 RequestMapping接口
+     * 获取接口URL加入请求白名单
+     */
+    public Set<String> scannerOpenApiRequestMappingUrlPatterns() {
+        var handlerMethods = handlerMapping.getHandlerMethods();
+        var handlerMethodEntry = handlerMethods.entrySet();
+
+        //扫描Spring MVC HandlerMapping添加OpenApi注解接口并转换成UrlPattern
+        return handlerMethodEntry.stream()
+                .filter(entry -> {
+                    var handlerMethod = entry.getValue();
+                    return AnnotationUtils.findAnnotation(handlerMethod.getMethod(), OpenApi.class) != null
+                            || AnnotationUtils.findAnnotation(handlerMethod.getBeanType(), OpenApi.class) != null;
+                })
+                .map(entry -> {
+                    var requestMappingInfo = entry.getKey();
+                    return requestMappingInfo
+                            .getPatternsCondition()
+                            .getPatterns();
+                })
+                .flatMap(Collection::stream)
+                .map(pattern -> RegExUtils.replaceAll(pattern, PATH_VARIABLE_PATTERN, "*"))
+                .collect(Collectors.toSet());
+    }
+
+}
