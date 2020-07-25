fc4e5d0fbf22aff3072b7bd0f0906c021c6621f7
Devifish <devifish@outlook.com>
2020-07-25 18:34:01 +0800

    remove: 去除MethodSecurityConfiguration

--- fish-common/fish-common-security/src/main/java/cn/devifish/cloud/common/security/config/WebSecurityConfiguration.java
+++ fish-common/fish-common-security/src/main/java/cn/devifish/cloud/common/security/config/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  */
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     /**
