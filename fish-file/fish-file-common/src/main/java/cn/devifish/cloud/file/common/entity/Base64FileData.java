5014ceac820d67a92b9ffa2c168a3d6d9614a479
Devifish <devifish@outlook.com>
2020-08-26 16:47:59 +0800

    change: 一些调整

--- fish-file/fish-file-common/src/main/java/cn/devifish/cloud/file/common/entity/Base64FileData.java
+++ fish-file/fish-file-common/src/main/java/cn/devifish/cloud/file/common/entity/Base64FileData.java
@@ -3,6 +3,7 @@ package cn.devifish.cloud.file.common.entity;
 import cn.devifish.cloud.common.core.constant.RegexpConstant;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
@@ -20,6 +21,7 @@ public class Base64FileData implements Serializable {
     private String filename;
 
     /** Base64内容 **/
+    @NotEmpty
     @Pattern(regexp = RegexpConstant.BASE64_FILE)
     private String content;
 
